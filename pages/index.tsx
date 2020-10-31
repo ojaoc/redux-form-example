9a8deb465e3b0a9767ea51cb965518f03f00a2f6
ojaoc <correia.jafonso@gmail.com>
2020-10-31 16:55:01 +0000

    need to integrate material ui for ssr/ssg

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import Box from "@material-ui/core/Box";
+import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+
+const WhiteTextTypography = withStyles({
+    root: {
+        color: "#FFFFFF",
+    },
+})(Typography);
 
 export default function Home() {
     return (
-        <Typography variant="h1" gutterBottom>
-            h1. Heading
-        </Typography>
+        <Box display="flex" justifyContent="center">
+            <WhiteTextTypography variant="h3" gutterBottom>
+                Form example
+            </WhiteTextTypography>
+        </Box>
     );
 }
