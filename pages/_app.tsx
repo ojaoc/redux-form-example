a2b48e03de9bb41918b3d3c35bcc410d21a1f15e
ojaoc <correia.jafonso@gmail.com>
2020-10-31 21:58:34 +0000

    need to implement custom document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../styles/global.css";
 import { Provider } from "react-redux";
@@ -8,6 +8,14 @@ import rootReducer from "../reducers";
 const store = createStore(rootReducer);
 
 function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector("#jss-server-side");
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
         <Provider store={store}>
             <Component {...pageProps} />
