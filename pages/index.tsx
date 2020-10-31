c7f99b0d3d3bef99edd38df34bb34db4456ce863
ojaoc <correia.jafonso@gmail.com>
2020-10-31 12:49:40 +0000

    Bootraping project

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Typography from "@material-ui/core/Typography";
+
+export default function Home() {
+    return (
+        <Typography variant="h1" gutterBottom>
+            h1. Heading
+        </Typography>
+    );
+}
