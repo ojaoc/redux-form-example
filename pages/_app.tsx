4fccfb283d346fbb3e87db8ee87b49105731d10b
ojaoc <correia.jafonso@gmail.com>
2020-10-31 13:09:56 +0000

    Added redux config and redux form reducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
 import "../styles/global.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "../reducers";
+
+const store = createStore(rootReducer);
 
 function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <Provider store={store}>
+            <Component {...pageProps} />
+        </Provider>
+    );
 }
 
 export default App;
